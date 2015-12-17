5d02c6d06e0c1d746f1574deeff7e7f3557a3b46
cch5ng <cch5ng@gmail.com>
2015-12-17 00:08:11 -0800

    fix: update server startup logic to use jquery .text() instead of .contents(). client now pulling data from mongo.

--- App.jsx
+++ App.jsx
@@ -1,66 +1,32 @@
 //App.jsx
 
 App = React.createClass({
-	getInitialState: function() {
-		return {data: []};
-	},
-	componentDidMount: function() {
-//replace ajax call
-		$.ajax({
-        	method: 'GET',
-        	url: this.props.url,
-        	contentType: 'text/plain',
-        	dataType: 'xml',
-        	success: function(data) {
-        		var feeds = data.querySelectorAll('item');
-        		var numFeeds = feeds.length; //15;
-        		//console.log('feeds: ' + feeds);
-        		var feedsAr = [];
-        		var prefix = 'npr';
+	mixins: [ReactMeteorData],
 
-        		for (var i = 0; i < numFeeds; i++) {
-        			var feedObj = {};
-//TODO the npr id value is not good because if websockets is used, the array idx is not going to be consistent
-//across requests for the same story
-//the link is going to be consistent so maybe just prepend it with 'npr'
-        			feedObj.id = prefix + feeds[i].getElementsByTagName('link')[0].innerHTML;
-        			feedObj.title = feeds[i].getElementsByTagName('title')[0].innerHTML;
-        			feedObj.description = feeds[i].getElementsByTagName('description')[0].innerHTML;
-        			feedObj.pubDate = feeds[i].getElementsByTagName('pubDate')[0].innerHTML;
-        			feedObj.link = feeds[i].getElementsByTagName('link')[0].innerHTML;
-        			//console.log('title: ' + feedObj.title);
-        			//console.log('desc: ' + feedObj.description);
-        			feedsAr.push(feedObj);
-        		}
-        		if (this.isMounted()) {
-					this.setState({data: feedsAr});
-				}
-				console.log(this.state.data[0].length);
-				console.log(typeof this.state.data[0]);
-				console.log(typeof this.state.data[0]);
-
-        		//console.log('feedsAr: ' + feedsAr);
-      		}.bind(this),
-      		error: function(xhr, status, err) {
-        		console.error(this.props.url, status, err.toString());
-      		}.bind(this)
-        });
+	getMeteorData() {
+		return {
+			nprfeeds: Nprfeeds.find({}).fetch()
+		};
 	},
-	// componentDidMount: function() {
-	// 	this.loadFeedsFromServer();
-	// 	//setInterval(this.loadCommentsFromServer, this.props.pollInterval);
-	// },
+
+	renderNprFeeds() {
+		return this.data.nprfeeds;//.map((nprfeed) => {
+			//return <Feed key={nprfeed._id} feed={nprfeed}></Feed>
+			//);
+		//}) 
+	},
+
 	render: function() {
 		return (<div className="nprFeed container">
 					{/*<button className="btnNpr">Play</button>*/}
 					<div className="row">
 						<h3 className='h3-npr'>NPR News Feed {/*this.state.data[0].link ? 'NPR News Feed' : ''*/}</h3>
 					</div>
-					<FeedList data={this.state.data} />
+					<FeedList data={this.data.nprfeeds} />
 				</div>
 		);
 	}
-})
+});
 
 var Feed = React.createClass({
 	render: function() {
--- server/startup.js
+++ server/startup.js
@@ -1,40 +1,72 @@
 //startup.js
 
-// Meteor.methods({
+Meteor.startup(function() {
 // 	getUrl: function(url) {
 		var url = 'http://www.npr.org/rss/rss.php?id=1001';
-		var nprFeeds = [];
 		var result = HTTP.call("GET", url, function(error, result) {
 			if (!error) {
 //this part works
 				//console.log('result content: ' + result.content);
 				console.log('type of: ' + typeof result.content);
+				//console.log('size NprFeeds collection: ' + NprFeeds.find().count());
+
 				$ = cheerio.load(result.content, {
 				    normalizeWhitespace: true,
 				    xmlMode: true
 				});
 				//console.log($);
-				var feeds = $('item');
+				//var feeds = $('item');
 				//console.log('length feeds: ' + feeds.length);
-
+				var nprFeedsAr = [];
 				var prefix = 'npr';
 
 				$('item').each(function(idx, element) {
-					var feedObj = {};
-					feedObj._id = prefix + $(this).find('link').contents();
-					console.log('id: ' + feedObj._id);
-        			feedObj.title = $(this).find('title').contents();
-        			console.log('title: ' + feedObj.title);
-         			feedObj.description = $(this).find('description').contents();
-         			console.log('description: ' + feedObj.description);
-         			feedObj.pubDate = $(this).find('pubDate').contents();
-         			console.log('pubDate: ' + feedObj.pubDate);
-         			feedObj.link = $(this).find('link').contents();
-         			console.log('link: ' + feedObj.link);
-         			nprFeeds.push(feedObj);
-				});
+					//setTimeout(function() {
+						var feedObj = {};
+						var feedId;
+	         			feedObj.link = $(this).find('link').text();//.contents();
+						feedObj._id = prefix + feedObj.link;
+						console.log('id: ' + feedObj._id);
+						console.log('type of id: ' + typeof feedObj._id);
+	        			feedObj.title = $(this).find('title').text();//.contents();
+	        			console.log('title: ' + feedObj.title);
+	         			feedObj.description = $(this).find('description').text();//.contents();
+	         			console.log('description: ' + feedObj.description);
+	         			//feedObj.pubDate = $(this).find('pubDate').contents();
+	         			//console.log('pubDate: ' + feedObj.pubDate);
+	         			//console.log('link: ' + feedObj.link);
+	         			//var feedId = NprFeeds.insert(feedObj);
+	         			console.log('feedObj: ' + feedObj);
+	         			//nprFeedsAr.push(feedObj);
+	         			//verify that the feed is new
+	         			if (!Nprfeeds.find(feedObj._id)) {
+		         			Nprfeeds.insert(feedObj, function(er, id) {
+		         				console.log('id: ' + id);
+		         				if (!er) {
+									console.log('insert');
+									console.log('size NprFeeds collection: ' + Nprfeeds.find().count());
+		         				} else {
+		         					console.log('error: ' + er);
+		         				}
+							});
+	         			}
 
-				console.log('length nprFeeds: ' + nprFeeds.length);
+					//}, 0);
+				});
+				console.log('length nprFeedsAr: ' + nprFeedsAr.length);
+				console.log('size NprFeeds collection: ' + Nprfeeds.find().count());
+
+				// for (var i = 0; i < nprFeedsAr.length; i++) {
+				 	// var feedId = Nprfeeds.insert(nprFeedsAr[0], function(er, id) {
+				 	// 	if (!er) {
+					 // 		console.log('insert');
+					 // 		console.log('size NprFeeds collection: ' + Nprfeeds.find().count());
+				 	// 	} else {
+				 	// 		console.log('err insert: ' + er);
+				 	// 	}
+				 	// });
+				// }
+
 
 			} else {
 				console.log('error: ' + error);
@@ -43,4 +75,4 @@
 //this part does not seem to have any results on client side
 // 		return result.content; //result;
 // 	}
-// });
+});
