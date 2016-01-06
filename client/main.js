9357dcec199d133a25018f9359a9818b96cd17a5
cch5ng <cch5ng@gmail.com>
2016-01-05 20:52:57 -0800

    feat: enabled audio player again now using web speech, speechSynthesis api directly not using voice elements b/c of meteor conflict

--- client/main.js
+++ client/main.js
@@ -1,19 +1,28 @@
 Meteor.startup(function() {
-  //TODO if I wait for DOMContentLoaded, this logic never appears to run but if I disable this check,
-  //element functions from voice elements/polymer never appear to get recognized
-  //document.addEventListener('DOMContentLoaded', function() {
-    var element = document.querySelector('#player'); //('voice-player');
+    // var element = document.querySelector('#player'); //('voice-player');
   //TODO how to handle this for multiple feeds
     var counter = 0;
     var feed;
     var feedNodes;
     var feedNodesLength;
     var audioStates = ['playing', 'stopped', 'paused'];
-    //audioState can be 
     var audioState = audioStates[1];
-    console.log('audioState: ' + audioState);
-    console.log('element: ' + element);
-    console.log('element inner html' + element.innerHTML);
+    // console.log('audioState: ' + audioState);
+    // console.log('element: ' + element);
+    // console.log('element inner html' + element.innerHTML);
+
+//setup audio player (web audio api speech synthesis)
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+//this doesn't seem to have any effect on chrome
+    msg.voice = voices[10]; // Note: some voices don't support altering params
+    msg.voiceURI = 'native';
+    msg.volume = 1; // 0 to 1
+    msg.rate = 1; // 0.1 to 10
+    msg.pitch = 1; //0 to 2
+//    msg.text = 'Hello World';
+    msg.lang = 'en-US';
+
 
     //added a cancel() call because otherwise the play/speak() was not applying to the first feed item
     //maybe the voice-element state was cached from previous session
@@ -27,8 +36,14 @@ Meteor.startup(function() {
     //       console.log('audioState: ' + audioState);
       //text += feedNodes[i].querySelector('p').innerHTML;
       //console.log('text: ' + text);
-      element.setAttribute('text', feed);
-      element.speak();
+
+//comment out voice elements calls
+      // element.setAttribute('text', feed);
+      // element.speak();
+      
+//set text for speechsynthesis
+      msg.text = feed;
+      window.speechSynthesis.speak(msg);
       console.log('playing audio');
       audioState = audioStates[0];
     }
@@ -47,7 +62,9 @@ Meteor.startup(function() {
       //if audio paused
         if (counter > 0) {
           //NOTE resume only resumes feed[0]; but audio does not play through the list
-          element.resume();
+          window.speechSynthesis.resume();
+
+          //element.resume();
           // console.log('counter: ' + counter);
           // console.log('audioState: ' + audioState);
         //NOTE cancel increments counter but makes the audio play through the list
@@ -66,52 +83,79 @@ Meteor.startup(function() {
 
   //TODO click listener for audio Pause button
     $('.fa-pause').click( function() {
-      element.pause();
+      window.speechSynthesis.pause();
+
+      //element.pause();
       audioState = audioStates[2];
       console.log('audioState: ' + audioState);
     });
 
+  //disabled because cancel was giving unpredictable behavior
   //TODO click listener for audio Stop button (audio cancel)
-    $('.fa-stop').click( function() {
-      element.cancel();
+    //$('.fa-stop').click( function() {
+      //element.cancel();
       //counter = feedNodesLength - 1;
-      element.setAttribute('text', '');
-      counter = 0;
-      audioState = audioStates[1];
+      //element.setAttribute('text', '');
+      //counter = 0;
+      //audioState = audioStates[1];
       //console.log('counter: ' + counter);
       //console.log('audioState: ' + audioState);
-    });
+    //});
 
   //TODO this event does not currently get triggered
-    element.addEventListener('end', function(e) {
-      console.log('finished playing one feed');
-      e.preventDefault();
+    // element.addEventListener('end', function(e) {
+    //   console.log('finished playing one feed');
+    //   e.preventDefault();
       //console.log('feedNodesLength - 1: ' + (feedNodesLength - 1).toString());
       //console.log('gets to end of feed num: ' + counter);
-      if (counter < feedNodesLength - 1) {
-        counter++; 
-        //console.log('counter: ' + counter);
-        playNextFeed();
-      } else if (counter === feedNodesLength - 1) {
+      // if (counter < feedNodesLength - 1) {
+      //   counter++; 
+      //   //console.log('counter: ' + counter);
+      //   playNextFeed();
+      // } else if (counter === feedNodesLength - 1) {
   //TODO would like to indicate that audio is looping again from the start 
         // element.pause();
         // element.setAttribute('text', 'restarting feed audio');
         // element.resume();
-        counter = 0;
-        playNextFeed();
-        //console.log('counter: ' + counter);
-      }
-    });
+    //     counter = 0;
+    //     playNextFeed();
+    //     //console.log('counter: ' + counter);
+    //   }
+    // });
 
-    element.addEventListener('start', function(e) {
-      e.preventDefault();
-    });
 
-    element.addEventListener('pause', function(e) {
-      e.preventDefault();
-    });
+    msg.onend = function(e) {
+        console.log('finished playing one feed');
+        e.preventDefault();
+        if (counter < feedNodesLength - 1) {
+            counter++;
+            //console.log('counter: ' + counter);
+            playNextFeed();
+        } else if (counter === feedNodesLength - 1) {
+            window.speechSynthesis.pause();
+            //comment out cancel because it seems like you cannot resume playing afterward
+            //window.speechSynthesis.cancel();
 
-    element.addEventListener('resume', function(e) {
-      e.preventDefault();
-    });
+            //msg.text = 'restarting feed audio';
+            //window.speechSynthesis.resume();
+            counter = 0;
+            //playNextFeed();
+        }
+
+      //console.log('Finished in ' + event.elapsedTime + ' seconds.');
+    };
+
+
+//not sure these necessary
+    // element.addEventListener('start', function(e) {
+    //   e.preventDefault();
+    // });
+
+    // element.addEventListener('pause', function(e) {
+    //   e.preventDefault();
+    // });
+
+    // element.addEventListener('resume', function(e) {
+    //   e.preventDefault();
+    // });
 });
